52e8fb293f02e5cdc47ec872920e05dce7bf6a16
Rythmat <teotaniwha@gmail.com>
2025-09-01 15:22:49 -0700

    no resend

--- src/lib/resend/index.ts
+++ src/lib/resend/index.ts
@@ -1,4 +1,4 @@
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 import { Env } from '@/constants/env';
 
-export const resend = new Resend(Env.get('RESEND_API_KEY'));
+// export const resend = new Resend(Env.get('RESEND_API_KEY'));
